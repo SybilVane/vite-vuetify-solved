3ee6c1ce1edaf8660d9651e9f516a96d49ea01de
Sybil <vanessa.zgn@gmail.com>
2023-04-19 23:48:14 +0200

    deleted useless files, add code to App and created FavDogCard component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,133 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import axios from 'axios';
+import FavDogCard from './components/FavDogCard.vue'
+export default {
+  components: {
+    FavDogCard
+  },
+  data() {
+    return {
+      currentDogLink: "https://images.dog.ceo/breeds/chihuahua/n02085620_3407.jpg",
+      favoriteDogs: [],
+    };
+  },
+  methods: {
+    loadNewDog() {
+      axios
+          .get("https://dog.ceo/api/breeds/image/random")
+          .then(response => {
+            this.currentDogLink = response.data.message;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    addToFavorites() {
+      this.favoriteDogs.push(this.currentDogLink)
+    },
+    removeFromFavorites(index) {
+      this.favoriteDogs.splice(index, 1)
+    },
+  },
+  computed: {
+    isAlreadyInFavorites() {
+      return this.favoriteDogs.includes(this.currentDogLink)
+    },
+  },
+  created() {
+    this.loadNewDog()
+  }
+};
 </script>
 
 <template>
-  <div>
-    <v-card text="Hi! I'm a Vuetify Card"></v-card>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main class="dogs-layout">
+      <v-container fill-height>
+        <div class="dogs-overlay">
+          <h1 class="display-2 text-xs-center">Choose your favorite dogs</h1>
+          <v-card class="dog-card">
+            <v-img height="400px" :src="currentDogLink"/>
+            <v-card-actions>
+              <v-spacer/>
+              <v-btn icon :disabled="isAlreadyInFavorites">
+                <span class="material-icons" @click="addToFavorites">favorite</span>
+              </v-btn>
+              <v-btn icon @click="loadNewDog">
+                <span class="material-icons">forward</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-container grid-list-md fluid>
+            <v-row>
+              <v-col cols="3" v-for="(pet, index) in favoriteDogs">
+                <FavDogCard :dog-image-src="pet" @remove="removeFromFavorites(index)"/>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<style>
+img {
+  max-width: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  padding-bottom: 15px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.dogs-layout {
+  width: 100%;
+  background: #fff url("https://github.com/FrontEndFoxes/projects/blob/main/petshop/images/bg3.jpg?raw=true") repeat center;
+}
+
+.dogs-overlay {
+  width: 100%;
+  padding: 20px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+@media (max-width: 768px) {
+  .dogs-overlay {
+    margin: 0;
+  }
+}
+
+.dog-card {
+  width: 100%;
+  max-width: 600px;
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
 }
 </style>
---
+++ src/components/FavDogCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    dogImageSrc: {
+      type: String
+    }
+  }
+};
+</script>
+
+<template>
+    <v-card class="dog-card">
+      <v-img height="150px" :src="dogImageSrc"/>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn icon @click="$emit('remove')">
+          <span class="material-icons">delete</span>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
